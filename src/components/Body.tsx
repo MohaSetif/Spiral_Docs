4d5f32b617408ceee93fc1972f33be2a8931e0b7
MohaSetif <BouroubaMohamed004@gmail.com>
2023-11-23 21:03:44 +0100

    0.0.5

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,10 +2,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLaravel, faReact, faVuejs } from '@fortawesome/free-brands-svg-icons'
 import faSvelte from '../assets/images/pngaaa.com-4915832-removebg-preview.png'
 import '../assets/css/Home.css'
+import Card from './Card';
 
 export default function Body() {
+  const cardsData = [
+    { icon: faLaravel, title: 'Laravel', content: 'Laravel backend' },
+    { icon: faReact, title: 'React', content: 'React frontend' },
+    { icon: faVuejs, title: 'Vue.js', content: 'Vue.js frontend' },
+    { icon: faSvelte, title: 'Svelte', content: 'Svelte frontend' }
+  ];
   return (
-    <div className="Home">
+    <>
+       <div className="Home">
         <div className="left_content">
           <h2>One Backend, Many <span className='frontends'>Frontends</span></h2>
           <h3>You're a Javascript fan? No problem, Build robust applications in PHP using Laravel as a backend. As a frontend, use React, Vue or even Svelte by pairing Laravel with Inertia or Typescript.</h3>
@@ -27,5 +35,16 @@ export default function Body() {
           </div>
         </div>
       </div>
+      <div id="cards">
+          {cardsData.map((card, index) => (
+              <Card
+                key={index}
+                icon={card.icon}
+                title={card.title}
+                content={card.content}
+              />
+          ))}
+      </div>
+    </>
   )
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,43 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { useEffect } from "react"
+
+interface CardProps{
+    icon: string | IconDefinition;
+    title: string;
+    content: string;
+  };
+
+export default function Card (props: CardProps) {
+    useEffect(() => {
+        document.getElementById("cards")?.addEventListener("mousemove", (e) => {
+          const cards = document.getElementsByClassName("card");
+          for (const card of cards) {
+            const rect = (card as HTMLElement).getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+    
+            (card as HTMLElement).style.setProperty("--mouse-x", `${x}px`);
+            (card as HTMLElement).style.setProperty("--mouse-y", `${y}px`);
+          }
+        });
+      }, []);
+
+    return (
+    <div className="card">
+        <div className="card-content">
+        <div className="card-image">
+            <i className={props.icon}></i>
+        </div>
+        <div className="card-info-wrapper">
+            <div className="card-info">
+            <i className="fa-duotone fa-apartment"></i>
+            <div className="card-info-title">
+                <h3>{props.title}</h3>  
+                <h4>{props.content}</h4>
+            </div>    
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
